58400fe2608455d6458547352e87aaa12d0fbc4a
bruno <bruno.bertossi@gmail.com>
2020-01-21 17:07:27 -0300

    tercer commit

--- prueba.sql
+++ prueba.sql
@@ -45,5 +45,23 @@ CREATE TABLE categorias(
     descripcion VARCHAR(200)
 );
 
+INSERT INTO categorias(id_categoria, categoria, descripcion) VALUES('1','Comida','Verduras, carne , lacteos, snacks, etc.');
+INSERT INTO categorias(id_categoria, categoria, descripcion) VALUES('2','bebestibles','Bebidas, Jugo, Agua, Helado');
+INSERT INTO categorias(id_categoria, categoria, descripcion) VALUES('3','drogas','Cigarros, Alcohol, cannabis(medicinal)');
 
 
+ALTER TABLE categorias ADD PRIMARY KEY (id_categoria);
+ALTER TABLE productos ADD PRIMARY KEY (id_productos);
+ALTER TABLE productos ADD CONSTRAINT constraintname UNIQUE (categoria);
+ALTER TABLE categorias ADD CONSTRAINT constraint_name UNIQUE (categoria);
+ALTER TABLE productos ADD FOREIGN KEY (categoria) REFERENCES categorias (categoria);
+
+
+INSERT INTO productos (id_productos, nombre_producto, descripcion_producto, precio_unidad, categoria) VALUES ('1','Pan','Pan horneado', '100','Comida');
+INSERT INTO productos (id_productos, nombre_producto, descripcion_producto, precio_unidad, categoria) VALUES ('2','Queso','Queso laminado', '150','Comida');
+INSERT INTO productos (id_productos, nombre_producto, descripcion_producto, precio_unidad, categoria) VALUES ('3','Leche','Leche sin lactosa', '200','bebestibles');
+INSERT INTO productos (id_productos, nombre_producto, descripcion_producto, precio_unidad, categoria) VALUES ('4','cigarros','cajetilla de 20', '350','drogas');
+INSERT INTO productos (id_productos, nombre_producto, descripcion_producto, precio_unidad, categoria) VALUES ('5','Pisco','CAPEL 35°', '400','drogas');
+INSERT INTO productos (id_productos, nombre_producto, descripcion_producto, precio_unidad, categoria) VALUES ('6','Lechuga','Escarola fresca', '100','Comida');
+INSERT INTO productos (id_productos, nombre_producto, descripcion_producto, precio_unidad, categoria) VALUES ('7','Pepsi','Pepsi 1.5lts.', '300','bebestibles');
+INSERT INTO productos (id_productos, nombre_producto, descripcion_producto, precio_unidad, categoria) VALUES ('8','Jugo de manzana','Jugo de manzana 100% natural', '300','bebestibles');
